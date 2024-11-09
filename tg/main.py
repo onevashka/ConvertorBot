049a6ce51be5fffd21fe2d3296573efea3718704
danila <danila7ivankov@gmail.com>
2024-11-10 02:27:50 +0700

    Create dir app and file router

--- tg/main.py
+++ tg/main.py
@@ -1,28 +1,20 @@
 import logging
+import asyncio
 import sys
 import os
-import asyncio
 from config import TOKEN
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from app.router import router
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+dp.include_router(router=router)
 
-
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Hello')
-
-
-@dp.message(Command('help'))
-async def cmd_help(message: Message):
-    await message.answer(f"Hello {message.from_user.username}!\nThis bot is made so that you can convert your file to the extension you need.\nRather, send him your file and press the button you need.")
 
 async def main():
     await dp.start_polling(bot)
