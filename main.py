619c33803a9464a825648445a1c7a17dcbc0e967
danila <danila7ivankov@gmail.com>
2024-11-10 23:57:44 +0700

    Create new file router and file main cd ..

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import logging
 import asyncio
-import sys
-import os
 from config import TOKEN
 from aiogram import Bot, Dispatcher
 from app.router import router
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-dp.include_router(router=router)
-
-
 async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router=router)
     await dp.start_polling(bot)
 
 
